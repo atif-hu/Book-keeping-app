9d2805552cc16aec31eb0f5cfd9e9caf9c664cd6
Atif Hussain <atifhussain28@gmail.com>
2022-11-11 15:33:17 +0530

    This is my first commit of Book- keeping-app

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express=require("express");
+const mongoose=require("mongoose")
+const app=express();
+
+//connect mongoose db
+mongoose().connect('mongodb+srv://atif:<LpKDtgT5JP7oCPmm>@cluster0.gjxjpka.mongodb.net/test')
+.then(()=>console.log("db connected"))
+.catch(err=>console.log(err));
+
+//Routes
+
+//User
+//Register
+app.post('/api/users/register',(req,res)=>{
+    res.send("user routes");
+})
+
+//login
+app.post('/api/users/login',(req,res)=>{
+    res.send("login routes");
+})
+
+//update user
+app.put('/api/users/update',(req,res)=>{
+    res.send("update routes");
+})
+
+//delete
+app.delete('/api/users/:id',(req,res)=>{
+    res.send("delete routes");
+})
+
+//fetch data
+app.get('/api/users',(req,res)=>{
+    res.send("fetch user");
+})
+
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
+
+
+//user name= atif
+//password = LpKDtgT5JP7oCPmm
