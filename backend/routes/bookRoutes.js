245ac0a7fa6b1c24570bdb6c4413d193f274c7b0
Atif Hussain <atifhussain28@gmail.com>
2022-11-27 01:05:28 +0530

    Created, fetched and updated books.

---
+++ backend/routes/bookRoutes.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const expressAsyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const Book = require('../models/Book');
+
+const bookRouter=express.Router();
+
+//creating a book   (Create)
+bookRouter.post('/',expressAsyncHandler(async(req,res)=>{
+    const book=await Book.create(req.body);
+    if(book){
+        res.status(200);
+        res.json(book);
+    }
+    else{
+        res.status(500);
+        throw new Error('Book creation failed');
+    }
+}))
+
+//Fetching all books (Read)
+bookRouter.get('/',expressAsyncHandler(async(req,res)=>{
+    const book=await Book.find({});
+    if(book){
+        res.status(200);
+        res.json(book);
+    }
+    else{
+        res.status(500);
+        throw new Error('Book fetching failed');
+    }
+}))
+
+bookRouter.put('/:id',
+    
+    expressAsyncHandler(async (req,res)=>{
+    const book=await Book.findById(req.params.id);
+    
+    if(book){
+        const updatedBook=await Book.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true});
+        res.status(200);
+        res.json(updatedBook);
+    }
+    else{
+        res.status(500);
+        throw new Error('Book updation failed');    
+    }
+}))
+
+module.exports=bookRouter;
