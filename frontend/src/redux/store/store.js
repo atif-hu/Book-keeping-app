6330c2b137de9356862ecad45e28269d4f60ba7f
Atif Hussain <atifhussain28@gmail.com>
2022-12-25 01:49:25 +0530

    Created store redux

---
+++ frontend/src/redux/store/store.js
@@ -0,0 +1,17 @@
+import {legacy_createStore as createStore,combineReducers,applyMiddleware} from 'redux'
+import thunk from "redux-thunk";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createBookReducer } from '../reducers/books/createBookReducer';
+
+const middlewares=[thunk];
+
+const reducer= combineReducers({
+    bookCreated:createBookReducer,
+});
+
+const store=createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+export {store};
